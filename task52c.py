537bd092104a4226de0d287d947a27dca61d616f
harshdeep singh <106699976+harshdeep-s@users.noreply.github.com>
2022-11-08 22:34:02 +0530

    Add files via upload

--- task52c.py
+++ task52c.py
@@ -6,76 +6,78 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(11, GPIO.OUT)
 GPIO.setup(12, GPIO.OUT)
 GPIO.setup(13, GPIO.OUT)
-GPIO.output(12, GPIO.LOW);
-GPIO.output(13, GPIO.LOW);
-GPIO.output(11, GPIO.LOW);
 
-# GUI DEFINITIONS
+GPIO.output(12, GPIO.LOW)
+GPIO.output(13, GPIO.LOW)
+GPIO.output(11, GPIO.LOW)
+
+# GUI DEFINITIONS8
 
 win = Tk()
+win.geometry("250x200")
 win.title("LED TOGGLER")
-myfont = tkinter.font.Font(family = "Helvetica", size = 12, weight= "bold")
+myfont = tkinter.font.Font(family = "Helvetica", size = 12, weight= "bold")     
 
-
-def ledToggle1():
-    if(GPIO.input(11)):
-        GPIO.output(11, GPIO.LOW);
-        ledButton1["text"] = "Turn LED1 on"
-           
-    else:
-#         GPIO.cleanup()
-        GPIO.output(12, GPIO.LOW);
-        GPIO.output(13, GPIO.LOW);
-        GPIO.output(11, GPIO.HIGH);
-        ledButton1["text"] = "Turn LED1 off"
-        ledButton2["text"] = "Turn LED2 on"
-        ledButton3["text"] = "Turn LED3 on"
-        
-def ledToggle2():
-    if(GPIO.input(12)):
-        GPIO.output(12, GPIO.LOW);
-        ledButton2["text"] = "Turn LED2 on"
-           
-    else:
-#         GPIO.cleanup()
-        GPIO.output(11, GPIO.LOW);
-        GPIO.output(13, GPIO.LOW);
-        GPIO.output(12, GPIO.HIGH);
-        ledButton2["text"] = "Turn LED2 off"
-        ledButton1["text"] = "Turn LED1 on"
-        ledButton3["text"] = "Turn LED3 on"
-        
-
-def ledToggle3():
-    if(GPIO.input(13)):
-        GPIO.output(13, GPIO.LOW);
-        ledButton3["text"] = "Turn LED3 on"
-           
-    else:
-#         GPIO.cleanup()
-        GPIO.output(11, GPIO.LOW);
-        GPIO.output(12, GPIO.LOW);
-        GPIO.output(13, GPIO.HIGH);
-        ledButton3["text"] = "Turn LED3 off"
-        ledButton1["text"] = "Turn LED1 on"
-        ledButton2["text"] = "Turn LED2 on"
-        
+num = IntVar()   
         
 def Exitwin():
     GPIO.cleanup()
     win.destroy()
+
+def led():
+    value = num.get()
+    if(value == 1):
+        if(GPIO.input(11)):
+            GPIO.output(11, GPIO.LOW)
+            ledButton1["text"] = "Turn green LED on"
+           
+        else:
+            GPIO.output(12, GPIO.LOW)
+            GPIO.output(13, GPIO.LOW)
+            GPIO.output(11, GPIO.HIGH)
+            ledButton1["text"] = "Turn green LED off"
+            ledButton2["text"] = "Turn blue LED on"
+            ledButton3["text"] = "Turn red LED on"
+
+    elif(value == 2):
+        if(GPIO.input(12)):
+            GPIO.output(12, GPIO.LOW)
+            ledButton2["text"] = "Turn blue LED on"
+           
+        else:
+            GPIO.output(11, GPIO.LOW)
+            GPIO.output(13, GPIO.LOW)
+            GPIO.output(12, GPIO.HIGH)
+            ledButton2["text"] = "Turn blue LED off"
+            ledButton1["text"] = "Turn green LED on"
+            ledButton3["text"] = "Turn red LED on"
+
+    elif(value == 3):
+        if(GPIO.input(13)):
+            GPIO.output(13, GPIO.LOW)
+            ledButton3["text"] = "Turn red LED on"
+        else:
+            GPIO.output(11, GPIO.LOW)
+            GPIO.output(12, GPIO.LOW)
+            GPIO.output(13, GPIO.HIGH)
+            ledButton3["text"] = "Turn red LED off"
+            ledButton1["text"] = "Turn green LED on"
+            ledButton2["text"] = "Turn blue LED on"
+
+
         
 # WIDGETS
-ledButton1 = Button(win, text = 'Turn led1 on', font = myfont, command = ledToggle1, bg = 'green', height = 1, width = 24)
-ledButton1.grid(row=0, column = 1)
+ledButton1 = Radiobutton(win, text = 'Turn green LED on',indicatoron = 0, font = myfont, variable=num, value = 1, command = led, height = 2, width = 20)
+ledButton1.place(relx = 0.5, rely = 0.1, anchor = CENTER)
 
-ledButton2 = Button(win, text = 'Turn led2 on', font = myfont, command = ledToggle2, bg = 'blue', height = 1, width = 24)
-ledButton2.grid(row=1, column = 1)
+ledButton2 = Radiobutton(win, text = 'Turn blue LED on',indicatoron = 0, font = myfont, variable=num, value = 2, command = led, height = 2, width = 20)
+ledButton2.place(relx = 0.5, rely = 0.3, anchor = CENTER)
+
+ledButton3 = Radiobutton(win, text = 'Turn red LED on',indicatoron = 0, font = myfont,variable = num, value = 3, command = led, height = 2, width = 20)
+ledButton3.place(relx = 0.5, rely = 0.5, anchor = CENTER)
 
 
-ledButton3 = Button(win, text = 'Turn led3 on', font = myfont, command = ledToggle3, bg = 'red', height = 1, width = 24)
-ledButton3.grid(row=2, column = 1)
+exitbutton = Radiobutton(win, text = 'Exit',indicatoron = 0, font = myfont, command = Exitwin, height = 2, width = 8)
+exitbutton.place(relx = 0.5, rely = 0.8, anchor = CENTER)
 
-
-exitbutton = Button(win, text = 'Exit', font = myfont, command = Exitwin, bg = 'blue', height = 1, width = 8)
-exitbutton.grid(row = 3, column = 1)
+win.mainloop()
